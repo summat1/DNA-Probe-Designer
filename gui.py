b9945e45f345030edb78505196defdf265b1e12f
Shivesh <shiveshummat@gmail.com>
2023-12-04 20:40:06 -0800

    update gui

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, QLabel, QLineEdit
 import sys
 import subprocess
 import os
@@ -9,37 +9,49 @@ class DNAProbeDesigner(QMainWindow):
 
         # Main Window setup
         self.setWindowTitle("DNA Probe Designer")
-        self.setGeometry(100, 100, 400, 300)  # Adjusted for additional text
+        self.setGeometry(100, 100, 550, 300)  # Adjusted for additional text
 
         # Layout
         layout = QVBoxLayout()
         
         # Welcome Label
-        self.welcomeLabel = QLabel("Welcome to the DNA Probe Designer!", self)
+        self.welcomeLabel = QLabel("Welcome to the DNA Probe Designer! To get started, please complete the 3 setup steps below:", self)
         self.welcomeLabel.setWordWrap(True)
 
-        # Description Label
-        self.descriptionLabel = QLabel("Attach a FASTA file and then click 'Run' to proceed with Oligominer.", self)
-        self.descriptionLabel.setWordWrap(True)
+        # Step 1 - Select FASTA Attachment Label + Button
+        self.fastaLabel = QLabel("Step 1: Attach target sequence.", self)
+        self.fastaLabel.setWordWrap(True)
+        self.attachFastaFileBtn = QPushButton("Attach .fasta", self)
+        self.attachFastaFileBtn.clicked.connect(self.openFileNameDialog)
 
-        # Attach File Button
-        self.attachFileBtn = QPushButton("Attach File", self)
-        self.attachFileBtn.clicked.connect(self.openFileNameDialog)
+        # Step 2 - Specify Bowtie Indices Label + Button
+        self.bowtieLabel = QLabel("Step 2: Select path to Bowtie indices.", self)
+        self.bowtieLabel.setWordWrap(True)
+        self.selectBowtieDir = QPushButton("Specify path to indices", self)
+        self.selectBowtieDir.clicked.connect(self.selectBowtieDirectory)
 
-        # In the __init__ method of DNAProbeDesigner
+        # Step 3 - Specify Bowtie Filename
+        self.bowtieName = QLabel("Step 3: Specify Bowtie filename.", self)
+        self.bowtieName.setWordWrap(True)
+        self.typeBowtieName = QLineEdit(self)
+        self.typeBowtieName.setPlaceholderText("Enter Bowtie2 name")
+        self.typeBowtieName.textChanged.connect(self.updateBowtieIndexName)
+
+
+        # Step 4 - Select Output Directory Label + Button
+        self.outputDirLabel = QLabel("Output Directory: Not selected", self)
+        self.outputDirLabel.setWordWrap(True)
         self.outputDirBtn = QPushButton("Select Output Directory", self)
         self.outputDirBtn.clicked.connect(self.selectOutputDirectory)
-        layout.addWidget(self.outputDirBtn)
-
-        # Add a label to display the selected directory
-        self.outputDirLabel = QLabel("Output Directory: Not selected", self)
-        layout.addWidget(self.outputDirLabel)
-
-        # Variable to store the output directory path
+        
+        # Variable to store the paths
+        self.fastaFilePath = ""
+        self.bowtieDirPath = ""
+        self.bowtieIndices = ""
         self.outputDirPath = ""
     
         # Run Button
-        self.runBtn = QPushButton("Run", self)
+        self.runBtn = QPushButton("Run Probe Design", self)
         self.runBtn.clicked.connect(self.runScript)
 
         # Status Label
@@ -47,8 +59,19 @@ class DNAProbeDesigner(QMainWindow):
 
         # Adding widgets to the layout
         layout.addWidget(self.welcomeLabel)
-        layout.addWidget(self.descriptionLabel)
-        layout.addWidget(self.attachFileBtn)
+
+        layout.addWidget(self.fastaLabel)
+        layout.addWidget(self.attachFastaFileBtn)
+
+        layout.addWidget(self.bowtieLabel)
+        layout.addWidget(self.selectBowtieDir)
+
+        layout.addWidget(self.bowtieName)
+        layout.addWidget(self.typeBowtieName)
+
+        layout.addWidget(self.outputDirLabel)
+        layout.addWidget(self.outputDirBtn)
+
         layout.addWidget(self.runBtn)
         layout.addWidget(self.statusLabel)
 
@@ -57,13 +80,20 @@ class DNAProbeDesigner(QMainWindow):
         centralWidget.setLayout(layout)
         self.setCentralWidget(centralWidget)
 
-        # Variable to store the file path
-        self.filePath = ""
-
     def openFileNameDialog(self):
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select a FASTA file", "", "FASTA Files (*.fasta);;All Files (*)")
-        if fileName:
-            self.filePath = fileName
+        fastaFileName, _ = QFileDialog.getOpenFileName(self, "Select a FASTA file", "", "FASTA Files (*.fasta);;All Files (*)")
+        if fastaFileName:
+            self.fastaFilePath = fastaFileName
+            self.fastaLabel.setText(f"FASTA attachment: {fastaFileName}")
+
+    def selectBowtieDirectory(self):
+        bowtieDir = QFileDialog.getExistingDirectory(self, "Select Directory")
+        if bowtieDir:
+            self.bowtieDirPath = bowtieDir
+            self.bowtieLabel.setText(f"Bowtie Directory: {bowtieDir}")
+    
+    def updateBowtieIndexName(self, text):
+        self.bowtieIndices = text
 
     def selectOutputDirectory(self):
         outputDir = QFileDialog.getExistingDirectory(self, "Select Directory")
@@ -71,29 +101,46 @@ class DNAProbeDesigner(QMainWindow):
             self.outputDirPath = outputDir
             self.outputDirLabel.setText(f"Output Directory: {outputDir}")
 
-
     def runScript(self):
-        if self.filePath and self.outputDirPath:
-            outputFile = os.path.join(self.outputDirPath, 'blockParse-output')
+        if self.fastaFilePath and self.bowtieDirPath and self.bowtieIndices and self.outputDirPath :
+            fastqOutputFile = os.path.join(self.outputDirPath, 'blockParse-output')
+            samFile = os.path.join(self.outputDirPath, 'bowtie-output')
+
+            # fastq file generation
             try:
                 command = [
                     "python", "Oligominer/blockParse.py",
-                    "-f", self.filePath,
-                    "-o", outputFile,
-                    # Add other necessary arguments
+                    "-f", self.fastaFilePath,
+                    "-o", fastqOutputFile,
+                    # other arguments could go here
                 ]
                 subprocess.run(command, check=True)
-                self.statusLabel.setText("Run Successful. Please check Output Folder specified for FASTQ file.")
             except subprocess.CalledProcessError as e:
                 self.statusLabel.setText(f"Error: {e}")
+
+            try:
+                command = [
+                "bowtie2",
+                "-x", self.bowtieIndices,  # Use the user-specified index
+                "-U", f"{fastqOutputFile}.fasta",  # Input FASTQ file
+                "--no-hd", "-t", "-k", "2", "--local",
+                "-D", "20", "-R", "3", "-N", "1", "-L", "20",
+                "-i", "C,4", "--score-min", "G,1,4",
+                "-S", samFile  # Output SAM file
+                ]
+                subprocess.run(command, check=True)
+            except subprocess.CalledProcessError as e:
+                self.statusLabel.setText(f"Error in bowtie2: {e}")
         else:
-            if not self.filePath:
-                self.statusLabel.setText("No File Selected!")
+            if not self.fastaFilePath:
+                self.statusLabel.setText("No Sequence Selected!")
+            elif not self.bowtieDirPath:
+                self.statusLabel.setText("Bowtie Indices Directory Not Selected!")
+            elif not self.bowtieIndices:
+                self.statusLabel.setText("Bowtie Indices Name Not Specified!")
             elif not self.outputDirPath:
                 self.statusLabel.setText("Output Directory Not Selected!")
-
-        
-
+            
 # Main loop
 app = QApplication(sys.argv)
 mainWindow = DNAProbeDesigner()
